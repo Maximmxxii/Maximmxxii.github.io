7468531a3cfe5d1eaf42bbb69434a8fd9ae51604
maximmxxii <maximilianommiv@gmail.com>
2023-07-25 22:52:19 -0500

    Añadiendolo al repo

--- app.js
+++ app.js
@@ -19,3 +19,16 @@ for(var x = 0; x<links.length;x++){
         menu_visible = false;
     }
 }
+//Creo las barritas de una Habilidad particular Identificada con su Id
+function crearBarra(id_barra)
+for(i = 0; i <= 16; i++){
+    let div = document.createElement("div");
+    div.className = "e";
+    id_barra.appendChild(div);
+}
+//Selecciono todas las barras generales para luego maniputarllas
+let html = document.getElementById("html");
+crearBarra(html);
+let javascript = document.getElementById("javascript");
+crearBarra(javascript);
+let 
